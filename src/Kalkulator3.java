b393a809da32ba0f8996ffbcbf81ca69eb6e9cba
Paweł <pawcio574@interia.pl>
2025-04-20 19:30:47 +0200

    Update 2.1

--- src/Kalkulator3.java
+++ src/Kalkulator3.java
@@ -1,11 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-//Version 1.10
-
-import java.util.Scanner;
-=======
-=======
->>>>>>> Stashed changes
 // Version 2.1
 
 import javax.swing.*;
@@ -13,18 +5,14 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Kalkulator3  extends JFrame implements ActionListener {
+public class Kalkulator3 extends JFrame implements ActionListener {
 
     JLabel l_a, l_b, l_result;
     JButton b_adding, b_subtraction, b_multiplication, b_splitting, b_power, b_square_root, b_cbrt;
     JTextField t_a, t_b;
     double a, b, wynik;
 
-<<<<<<< Updated upstream
-    public Kalkulator3 () {
-=======
     public Kalkulator3() {
->>>>>>> Stashed changes
         setSize(800,600);
         setTitle("Kalkulator");
         setLayout(null);
@@ -100,66 +88,13 @@ public class Kalkulator3  extends JFrame implements ActionListener {
         t_b = new JTextField("");
         t_b.setBounds(50, 200, 50, 50);
         add(t_b);
-<<<<<<< Updated upstream
-=======
 
         UIManager.put("ToolTip.font", new Font("Arial", Font.PLAIN, 16));
     }
->>>>>>> Stashed changes
 
-        UIManager.put("ToolTip.font", new Font("Arial", Font.PLAIN, 16));
-    }
->>>>>>> Stashed changes
-
-public class Kalkulator3 {
 
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Podaj pierwszą liczbę");
-        double liczba1 = scan.nextDouble();
-        //potrzebne, jeśli wczytujemy napis po wczytaniu liczby
-        scan.nextLine();
-
-        //Pobranie od użytkownika liczby jakie działanie ma zostać wykonane
-        System.out.println("1 - Dodawanie \n2 - Odejmowanie \n3 - Mnożenie \n4 - Dzielenie ");
-        System.out.println("Wybierz jakie działanie chcesz wykonać:");
-        new Scanner(System.in);
-        int operator = scan.nextInt();
-
-        //Zabezpieczenie przed wprowadzaniem złej wartości, powyżej 4
-        while (operator>4){
-            System.out.println("Podałeś zły operator, podaj prawidłowy operator");
-            System.out.println("1 - Dodawanie \n2 - Odejmowanie \n3 - Mnożenie \n4 - Dzielenie ");
-            new Scanner(System.in);
-            operator = scan.nextInt();
-        }
-
-        System.out.println("Podaj drugą liczbę");
-        new Scanner(System.in);
-        double liczba2 = scan.nextDouble();
-        scan.close();
-
-        //Przełącznik do wykonywanych obliczeń
-        double suma = 0;
-        switch (operator) {
-            case 1:
-                suma = liczba1 + liczba2;
-                break;
-            case 2:
-                suma = liczba1 - liczba2;
-                break;
-            case 3:
-                suma = liczba1 * liczba2;
-                break;
-            case 4:
-                suma = liczba1 / liczba2;
-                break;
-        }
-        System.out.println("Wynik:" + suma);
-=======
-        Kalkulator3  main_window = new Kalkulator3 ();
+        Kalkulator3 main_window = new Kalkulator3();
         main_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         main_window.setVisible(true);}
 
@@ -212,6 +147,5 @@ public class Kalkulator3 {
                 return;
         }
         l_result.setText("Wynik:" + wynik);
->>>>>>> Stashed changes
     }
 }
