7495aa59b08b3e0f983167ac8fb29e738510e623
Paweł <pawcio574@interia.pl>
2025-04-20 19:24:18 +0200

    Update Kalkulator3.java

--- src/Kalkulator3.java
+++ src/Kalkulator3.java
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-//Version 1.10
-
-import java.util.Scanner;
-=======
-//version 2.2
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -74,55 +67,9 @@ public class Kalkulator3 extends JFrame implements ActionListener {
         t_b.setBounds(50, 200, 50, 50);
         add(t_b);
     }
->>>>>>> Stashed changes
 
 
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Podaj pierwszą liczbę");
-        double liczba1 = scan.nextDouble();
-        //potrzebne, jeśli wczytujemy napis po wczytaniu liczby
-        scan.nextLine();
-
-        //Pobranie od użytkownika liczby jakie działanie ma zostać wykonane
-        System.out.println("1 - Dodawanie \n2 - Odejmowanie \n3 - Mnożenie \n4 - Dzielenie ");
-        System.out.println("Wybierz jakie działanie chcesz wykonać:");
-        new Scanner(System.in);
-        int operator = scan.nextInt();
-
-        //Zabezpieczenie przed wprowadzaniem złej wartości, powyżej 4
-        while (operator>4){
-            System.out.println("Podałeś zły operator, podaj prawidłowy operator");
-            System.out.println("1 - Dodawanie \n2 - Odejmowanie \n3 - Mnożenie \n4 - Dzielenie ");
-            new Scanner(System.in);
-            operator = scan.nextInt();
-        }
-
-        System.out.println("Podaj drugą liczbę");
-        new Scanner(System.in);
-        double liczba2 = scan.nextDouble();
-        scan.close();
-
-        //Przełącznik do wykonywanych obliczeń
-        double suma = 0;
-        switch (operator) {
-            case 1:
-                suma = liczba1 + liczba2;
-                break;
-            case 2:
-                suma = liczba1 - liczba2;
-                break;
-            case 3:
-                suma = liczba1 * liczba2;
-                break;
-            case 4:
-                suma = liczba1 / liczba2;
-                break;
-        }
-        System.out.println("Wynik:" + suma);
-=======
         Kalkulator3 main_window = new Kalkulator3();
         main_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         main_window.setVisible(true);
@@ -171,7 +118,6 @@ public class Kalkulator3 extends JFrame implements ActionListener {
                 return;
         }
         l_result.setText("Wynik:" + wynik);
->>>>>>> Stashed changes
     }
 
     public boolean validateInputs() {
